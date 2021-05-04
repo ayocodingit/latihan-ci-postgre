116da3dc8b8710828a17764b1a38cfdaf20837d5
ayocodingit <ayocodingit@gmail.com>
2021-05-05 04:21:24 +0700

    dashboard chart

--- backend/app/Http/Controllers/V2/DashboardChartController.php
+++ backend/app/Http/Controllers/V2/DashboardChartController.php
@@ -10,58 +10,45 @@ class DashboardChartController extends Controller
 {
     public function registrasi(Request $request)
     {
-        $models = DashboardChart::where('nama', 'registrasi')
-                                ->where('tipe', $request->input('tipe', 'Daily'))
-                                ->where('where', $request->input('jenis', 'mandiri'))
-                                ->first();
-        return response()->json([
-            'label' => json_decode($models->label),
-            'value' => json_decode($models->data),
-        ]);
+        $tipe = $request->input('tipe', 'Daily');
+        $jenisRegistrasi = $request->input('jenis', 'mandiri');
+        return response()->json($this->getDashboardChart('registrasi', $tipe, $jenisRegistrasi));
     }
 
     public function sampel(Request $request)
     {
-        $models = DashboardChart::where('nama', 'sampel')
-                                ->where('tipe', $request->input('tipe', 'Daily'))
-                                ->first();
-        return response()->json([
-            'label' => json_decode($models->label),
-            'value' => json_decode($models->data),
-        ]);
+        $tipe = $request->input('tipe', 'Daily');
+        return response()->json($this->getDashboardChart('sampel', $tipe));
     }
 
     public function ekstraksi(Request $request)
     {
-        $models = DashboardChart::where('nama', 'ekstraksi')
-                                ->where('tipe', $request->input('tipe', 'Daily'))
-                                ->first();
-        return response()->json([
-            'label' => json_decode($models->label),
-            'value' => json_decode($models->data),
-        ]);
+        $tipe = $request->input('tipe', 'Daily');
+        return response()->json($this->getDashboardChart('ekstraksi', $tipe));
     }
 
     public function pcr(Request $request)
     {
-        $models = DashboardChart::where('nama', 'pcr')
-                                ->where('tipe', $request->input('tipe', 'Daily'))
-                                ->first();
-        return response()->json([
-            'label' => json_decode($models->label),
-            'value' => json_decode($models->data),
-        ]);
+        $tipe = $request->input('tipe', 'Daily');
+        return response()->json($this->getDashboardChart('pcr', $tipe));
     }
 
     public function positifNegatif(Request $request)
     {
-        $models = DashboardChart::where('nama', 'positif_negatif')
-                                ->where('tipe', $request->input('tipe', 'Daily'))
-                                ->where('where', $request->input('jenis', 'positif'))
+        $tipe = $request->input('tipe', 'Daily');
+        $hasilPemeriksaan = $request->input('jenis', 'positif');
+        return response()->json($this->getDashboardChart('positif_negatif', $tipe, $hasilPemeriksaan));
+    }
+
+    public function getDashboardChart($nameChart, $type, $where = NULL)
+    {
+        $models = DashboardChart::where('nama', $nameChart)
+                                ->where('tipe', $type)
+                                ->where('where', $where)
                                 ->first();
-        return response()->json([
+        return [
             'label' => json_decode($models->label),
             'value' => json_decode($models->data),
-        ]);
+        ];
     }
 }
